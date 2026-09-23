75f5ffc5cc12ede2eaad73e7e0c8c355ac65337e
Magda Melo <205521478+melosmag@users.noreply.github.com>
2026-09-23 08:37:12 -0300

    Atividade01 POO

---
+++ src/Main.java
@@ -0,0 +1,92 @@
+class Mensagem {
+
+    protected String campanha;
+    protected int quantidade;
+
+    public Mensagem(String campanha, int quantidade) {
+        this.campanha = campanha;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularCusto() {
+        return 0.0;
+    }
+
+    public String getCanal() {
+        return "Mensagem";
+    }
+
+    public void exibirResumo() {
+        System.out.printf("%s | %s | %d envios | R$ %.2f%n",
+                getCanal(), campanha, quantidade, calcularCusto());
+    }
+}
+
+class Email extends Mensagem {
+
+    public Email(String campanha, int quantidade) {
+        super(campanha, quantidade);
+    }
+
+    @Override
+    public double calcularCusto() {
+        double custo = quantidade * 0.02;
+
+        if (quantidade > 1000) {
+            custo = custo * 0.90;
+        } else {
+            custo = custo;
+        }
+
+        return custo;
+    }
+
+    @Override
+    public String getCanal() {
+        return "E-mail";
+    }
+}
+
+class SMS extends Mensagem {
+
+    public SMS(String campanha, int quantidade) {
+        super(campanha, quantidade);
+    }
+
+    @Override
+    public double calcularCusto() {
+        return quantidade * 0.15;
+    }
+
+    @Override
+    public String getCanal() {
+        return "SMS";
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Mensagem[] mensagens = {
+                new Email("boas-vindas", 2500),
+                new Email("promocao-agosto", 800),
+                new SMS("codigo-verificacao", 300)
+        };
+
+        double total = 0;
+
+        for (Mensagem mensagem : mensagens) {
+            mensagem.exibirResumo();
+            total += mensagem.calcularCusto();
+        }
+
+        System.out.printf("TOTAL DA FATURA: R$ %.2f%n", total);
+
+        if (total > 100) {
+            System.out.println("Atencao: fatura acima do limite contratado.");
+        } else {
+            System.out.println("Fatura dentro do limite.");
+        }
+    }
+}
